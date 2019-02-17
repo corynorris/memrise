9f04887b94ad7a42efbd3a728ecc684fbb710abf
Cory Norris <corynorris@gmail.com>
2019-02-16 22:58:10 -0500

    removed cache_manifiest from prod config

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,6 @@ config :memrise, MemriseWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT")],
   url: [scheme: "https", host: System.get_env("SERVER_URL"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
